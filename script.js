1c888854ed144fc25fa970d25e5eb8fe5b58bd2f
J0hn-4ndr3w <118709598+J0hn-4ndr3w@users.noreply.github.com>
2023-02-07 19:41:06 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ const header = document.querySelector(".arrow-up");
 window.addEventListener ("scroll", function() {
 	header.classList.toggle ("active", window.scrollY > 0);
 });
+
+const menuIcon = document.querySelector(".bx-menu");
+const menus = document.querySelector(".menus");
+
+menuIcon.addEventListener("click", () =>{
+	menuIcon.classList.toggle("bx-x");
+	menus.classList.toggle("open");
+})
+
+
+window.addEventListener("scroll", () =>{
+	menuIcon.classList.remove("bx-x");
+	menus.classList.remove("open");
+})
